c46f2180d391a9a14e064f842e81523942861e30
najibdevs <najibsoftwareeng@gmail.com>
2024-04-12 21:56:49 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML += `<p>New user id: ${id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+      });
+  }
+  
